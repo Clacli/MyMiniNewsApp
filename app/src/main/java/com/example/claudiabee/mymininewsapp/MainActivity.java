7c4e428425d32881c211726b8f3ce1a7421480f0
ClaudiaBee <claudiabellan.herb@gmail.com>
2018-07-21 20:32:19 +0200

    Build url

--- app/src/main/java/com/example/claudiabee/mymininewsapp/MainActivity.java
+++ app/src/main/java/com/example/claudiabee/mymininewsapp/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -26,6 +29,7 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<News>> {
 
+    public static final String STUDENT_API_KEY = "ADD_YOUR_API_KEY_HERE";
     // Bind resources with Butter Knife
     @BindString(R.string.no_internet_connection_message) String noInternetConnectionMessage;
     @BindString(R.string.no_article_found) String noArticleFound;
@@ -39,7 +43,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
      * URL to query for news data from the Guardian dataset
      */
     // Add a personal key at the end of the url String before trying the app
-    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?q=europe,culrure,politics&format=json&show-tags=contributor&api-key=ADD_YOUR_API_KEY_HERE";
+    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search";
 
     /**
      * Constant value for the earthquake loader
@@ -94,10 +98,32 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     // 1 First Loader callback to override onCreateLoader(){}
     @NonNull
     @Override
+    // onCreateLoader instantiates and returns a new Loader for the given ID
     public Loader<List<News>> onCreateLoader(int i, @Nullable Bundle bundle) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        // getString retrieves a String value from the preferences (topic). The second parameter is the
+        // default value for this preference.
+        String topic = sharedPreferences.getString(getString(R.string.settings_topic_key), getString(R.string.settings_topic_default));
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(GUARDIAN_REQUEST_URL);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the 'format=json'
+        uriBuilder.appendQueryParameter("q", topic);
+        uriBuilder.appendQueryParameter("format", "json");
+        uriBuilder.appendQueryParameter("show-tags", "contributor");
+        uriBuilder.appendQueryParameter("api-key", STUDENT_API_KEY);
+
         Log.d(LOG_TAG, "Verifying Loader behaviour: onCreateLoader");
-        // Create a new loader for the given URL
-        return new NewsLoader(this, GUARDIAN_REQUEST_URL);
+
+
+        //  Create a new loader for the given URL
+        return new NewsLoader(this, uriBuilder.toString());
     }
 
     // 2 Second Loader callbacks to override
